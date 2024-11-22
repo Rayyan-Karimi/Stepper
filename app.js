180d644abd3f467f54df8c66f8e566f10c8e11a8
rayyan6 <rayyan.karimi.30.3@mountblue.tech>
2024-11-22 21:28:25 +0530

    Completed the project, except for lines joining the steps.

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
 // script.js
 const steps = document.querySelectorAll(".step");
-const contents = document.querySelectorAll(".step-content");
+const lines = document.querySelectorAll(".line");
 const details = document.querySelectorAll(".step-detail")
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 
 let currentStep = 0;
 
-console.log(steps.length);
+/**
+ * Main functions
+ */
+prevButton.addEventListener("click", () => {
+    if (currentStep > 0) {
+        currentStep--;
+        updateStepper();
+        updateLinesBackward();
+    }
+});
+
+nextButton.addEventListener("click", () => {
+    if (currentStep < steps.length - 1) {
+        currentStep++;
+        updateStepper();
+        updateLinesForward();
+    }
+});
 
 function updateStepper() {
+    updateSteps();
+    updateStepDetails();
+    updateButtons();
+}
+
+updateStepper();
+
+
+/**
+ * Helper functions
+ */
+function updateSteps() {
     steps.forEach((step, index) => {
         step.classList.toggle("active", index === currentStep);
         step.classList.toggle("activated", index < currentStep);
+        if (index < currentStep) {
+            step.innerText = "✔"; // Checkmark
+        } else if (index === currentStep) {
+            step.innerText = index.toString(); // Reset to step number if not active
+        }
     });
-    contents.forEach((content, index) => {
-        if (index < 4)
-            content.classList.toggle("active", index === currentStep);
-    });
+}
+
+function updateLinesForward() {
+    console.log("Forw", currentStep);
+    if (currentStep > 0 && currentStep < 4)
+        lines[currentStep - 1].classList.toggle("activated");
+}
+
+function updateLinesBackward() {
+    console.log("Back", currentStep);
+    if (currentStep >= 0 && currentStep < 3)
+        lines[currentStep].classList.toggle("activated");
+}
+
+function updateStepDetails() {
     details.forEach((detail, index) => {
         detail.classList.toggle("active", index === currentStep)
     })
+}
 
+function updateButtons() {
     prevButton.disabled = currentStep === 0;
     nextButton.disabled = currentStep === steps.length - 1;
     if (currentStep >= steps.length - 2) {
@@ -30,19 +77,3 @@ function updateStepper() {
         nextButton.innerHTML = "Next";
     }
 }
-
-prevButton.addEventListener("click", () => {
-    if (currentStep > 0) {
-        currentStep--;
-        updateStepper();
-    }
-});
-
-nextButton.addEventListener("click", () => {
-    if (currentStep < steps.length - 1) {
-        currentStep++;
-        updateStepper();
-    }
-});
-
-updateStepper();
